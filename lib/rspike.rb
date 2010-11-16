d2354ab9b55090b8d58a852f8ef9d4740e1c3f38
Doug P <dougtko@gmail.com>
2010-11-16 02:45:34 -0500

    ...

--- lib/rspike.rb
+++ lib/rspike.rb
@@ -4,7 +4,30 @@
 
 require 'rspike.so'
 
-class Rspike
+class Spike
 Rspike::VERSION = "0.0.1"
 
+    def generate(script)
+        self.set_spike
+        self.init_fuzz
+
+        self.reset_fuzzstr
+
+        fuzzvarnum = 0
+        fuzzstrnum = 0
+
+        while(! self.didlastvar?) do
+            self.reset_fuzzstr
+            while(! self.didlastfuzzstr?) do
+                self.clear
+                self.parse(script)
+                yield self.get_databuf
+
+                fuzzstrnum += 1
+                self.inc_fuzzstr
+            end
+            fuzzvarnum += 1
+            self.inc_fuzzvar
+        end
+    end
 end

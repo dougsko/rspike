46ef119d2d16f51f9b33dda42bbf99bfae7bbdbc
doug <doug@debian.(none)>
2010-11-16 14:49:51 -0500

    ...

--- lib/rspike.rb
+++ lib/rspike.rb
@@ -4,6 +4,15 @@
 
 require 'rspike.so'
 
+class SpikeObject
+    attr_accessor :fuzzvarnum, :fuzzstrnum, :string
+    def initialize(fuzzvarnum, fuzzstrnum, string)
+        @fuzzvarnum = fuzzvarnum.to_s
+        @fuzzstrnum = fuzzstrnum.to_s
+        @string = string
+    end
+end
+
 class RSpike
     def generate(script)
         self.set_spike
@@ -19,7 +28,8 @@ class RSpike
             while(! self.didlastfuzzstr?) do
                 self.clear
                 self.parse(script)
-                yield self.get_databuf
+                so = SpikeObject.new(fuzzvarnum, fuzzstrnum, self.get_databuf)
+                yield so
 
                 fuzzstrnum += 1
                 self.inc_fuzzstr
